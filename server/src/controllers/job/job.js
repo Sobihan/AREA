7646c74df0203f6d9f7c874c80f7778931365a03
Adrien Moreau <adrien.moreau@epitech.eu>
2022-02-08 16:52:46 +0100

    [CLEAN]

--- server/src/controllers/job/job.js
+++ server/src/controllers/job/job.js
@@ -30,7 +30,6 @@ function isValidJson(data){
 const updateJob = (req, res, next) => {
     let isSuccess = true;
     let isSuccess_2 = true;
-    let isSuccess_3 = true;
     let is_failed = false;
     var interval = convertInt(req.body.interval, 10)
 
@@ -40,8 +39,6 @@ const updateJob = (req, res, next) => {
         console.log(e);
     })
     .then((user) => {
-        //console.log("test =", user.job);
-        //console.log("test =", user.job[(user.job.length - 1)].jobToken);
         if (user != null && (user.job.length - 1) >= 0) {
             var jobToken = user.job[(user.job.length - 1)].jobToken;
         }
@@ -73,234 +70,23 @@ const updateJob = (req, res, next) => {
                     });
                 }
                 else {
-                    job_extra.updateJob_extra_3(jobToken, actionArgs, reactionArgs, req)
+                    job_extra.updateJob_extra(jobToken, actionArgs, reactionArgs, req)
                     .then((data) => {
                         res.status(data.code).json(data.json);
                     });
                 }
             }
-
-
-/*
-            try {
-                //var isJobChecked = job_extra.checkGetJob(req.body.action, actionArgs, req.body.reaction, reactionArgs);
-                if (job_extra.checkGetJob(req.body.action, actionArgs, req.body.reaction, reactionArgs)) {
-                        job_extra.updateJob_extra_3(jobToken, actionArgs, reactionArgs, req)
-                        .then((data) => {
-                            let rslData = data
-                            //res.status(data.code).json(data.json);
-                        });
-                    }
-                    else {
-
-                        var rslData = {code:401, json:{
-                            success: false,
-                            body: 'Full Fail 1'
-                            }
-                        };
-                        /-*res.status(401).json({
-                            success: false,
-                            body: 'Full Fail'
-                        });*-/
-                    }
-            }
-            catch (error) {
-                var rslData = {code:401, json:{
-                    success: false,
-                    body: 'Full Fail 2'
-                    }
-                };
-                /-*res.status(401).json({
-                    success: false,
-                    body: 'Full Fail'
-                });*-/
-            }
-            finally {
-                res.status(rslData.code).json(rslData.json);
-            }
-*/
-            // //if (job_extra.checkGetJob(req.body.action, actionArgs, req.body.reaction, reactionArgs)) {
-            // if (isJobChecked) {
-            //     job_extra.updateJob_extra_3(jobToken, actionArgs, reactionArgs, req)
-            //     .then((data) => {
-            //         res.status(data.code).json(data.json);
-            //     });
-            // }
-            // else {
-            //     res.status(401).json({
-            //         success: false,
-            //         body: 'Full Fail'
-            //     });
-            // }
-
-            // job_extra.updateJob_extra_2(jobToken, actionArgs, reactionArgs, req)
-            // /*.catch((e) => {
-            //     isSuccess_2 = false;
-            //     console.log(e);
-            // })*/
-            // .then((data) => {
-
-            //     res.status(data.code).json(data.json);
-
-/*
-                console.log("code = ", data.code);
-                console.log("json = ", data.json);
-                res.status(200).json({
-                    success: true,
-                    body: 'Update of job done!'
-                });
-*/
-
-
-                /*if (isSuccess_2 == true) {
-                    console.log('job_extra.updateJob_extra SUCESSFUL');
-                    res.status(code).json(json);
-                }
-                else {
-                    console.log('job_extra.updateJob_extra FAIL');
-                    res.status(code).json(json);
-                }*/
-//            });
-
-
-/*
-            job_extra.updateJob_extra(jobToken, actionArgs, reactionArgs, req)
-            .catch((e) => {
-                isSuccess_2 = false;
-                console.log(e);
-            })
-            .then((code, json) => {
-                console.log("code = ", code);
-                console.log("json = ", json);
-                if (isSuccess_2 == true) {
-                    console.log('job_extra.updateJob_extra SUCESSFUL');
-                    res.status(code).json(json);
-                }
-                else {
-                    console.log('job_extra.updateJob_extra FAIL');
-                    res.status(code).json(json);
-                }
-            });
-*/
-
-            //var code, json = job_extra.updateJob_extra(jobToken, actionArgs, reactionArgs, req);
-
-            //res.status(code).json(json);
-
-
-
-            // job_extra.updateJob_extra(jobToken, actionArgs, reactionArgs)
-            // .catch((e) => {
-            //     console.log(e);
-            // })
-            // .then((arg_job) => {
-            //     //console.log('updateJob_extra Done Confirmed =', arg_job);
-            //     if (arg_job == true) {
-
-            //         job.findUniqueJob(jobToken)
-            //         .catch((e) => {
-            //             isSuccess_2 = false;
-            //             console.log(e);
-            //         })
-            //         .then((good_job) => {
-            //             if (isSuccess_2 == true && job_extra.checkGetJob(req.body.action, actionArgs, req.body.reaction, reactionArgs)) {
-            //                 console.log('updateJob SUCESSFUL');
-            //                 res.status(200).json({
-            //                     success: true,
-            //                     body: 'Update of job done!',
-            //                     good_job
-            //                 });
-
-            //                 scheduler.removeById(good_job.jobToken);
-
-            //                 const jobToLaunch = job_extra.getJob(req.body.action, actionArgs, req.body.reaction, reactionArgs);
-            //                 const job1 = new SimpleIntervalJob(
-            //                     { seconds: req.body.interval, runImmediately: req.body.runNow },
-            //                     jobToLaunch,
-            //                     good_job.jobToken
-            //                 );
-            //                 scheduler.addSimpleIntervalJob(job1);
-
-
-            //             }
-            //             else if (isSuccess_2 == true) {
-            //                 console.log('updateJob FAIL');
-            //                 scheduler.removeById(good_job.jobToken);
-            //                 job.deleteJob(req.header('authtoken'), good_job.jobToken)
-            //                 .catch((e) => {
-            //                     isSuccess_3 = false;
-            //                     console.log(e);
-            //                 })
-            //                 .then((user) => {
-            //                     if (isSuccess_3 == true) {
-            //                         console.log('good_job deleteJob SUCESSFUL');
-            //                         res.status(200).json({
-            //                             success: true,
-            //                             body: 'good_job deletion of job done!'
-            //                         });
-            //                     }
-            //                     else {
-            //                         console.log('good_job deleteJob FAIL');
-            //                         res.status(401).json({
-            //                             success: false,
-            //                             body: 'good_job deletion of job Failed'
-            //                         });
-            //                     }
-            //                 });
-
-            //             }
-            //             else {
-
-            //                 res.status(401).json({
-            //                     success: false,
-            //                     body: 'Update of job Failed'
-            //                 });
-
-            //             }
-            //         });
-            //     }
-            //     else {
-            //         console.log('updateJob_extra FAIL');
-            //         scheduler.removeById(jobToken);
-
-
-            //         job.deleteJob(req.header('authtoken'), jobToken)
-            //         .catch((e) => {
-            //             isSuccess_3 = false;
-            //             console.log(e);
-            //         })
-            //         .then((user) => {
-            //             if (isSuccess_3 == true) {
-            //                 console.log('updateJob_extra deleteJob SUCESSFUL');
-            //                 res.status(200).json({
-            //                     success: true,
-            //                     body: 'updateJob_extra deletion of job done!'
-            //                 });
-            //             }
-            //             else {
-            //                 console.log('updateJob_extra deleteJob FAIL');
-            //                 res.status(401).json({
-            //                     success: false,
-            //                     body: 'updateJob_extra deletion of job Failed'
-            //                 });
-            //             }
-            //         });
-            //     }
-            // });
-
-
-
         }
         else if (isSuccess == true && user != null && jobToken != '') {
             console.log('else if updateJob FAIL');
             scheduler.removeById(jobToken);
             job.deleteJob(req.header('authtoken'), jobToken)
             .catch((e) => {
-                isSuccess_3 = false;
+                isSuccess_2 = false;
                 console.log(e);
             })
             .then((user) => {
-                if (isSuccess_3 == true) {
+                if (isSuccess_2 == true) {
                     console.log('else if deleteJob SUCESSFUL');
                     res.status(200).json({
                         success: true,
@@ -373,65 +159,25 @@ const deleteJob = (req, res, next) => {
                                 });
                             }
                         });
-
-                        /*
-                        res.status(200).json({
-                            success: true,
-                            body: "deletion of job's reaction args Done!"
-                            //body: 'deletion of job done!'
-                        });
-                        */
                     }
                     else {
                         console.log('deleteReactionArgs FAIL');
                         res.status(401).json({
                             success: false,
                             body: "deletion of job's reaction args Failed!"
-                            //body: 'deletion of job Failed'
                         });
                     }
                 });
-
-            /*res.status(200).json({
-                success: true,
-                body: "deletion of job's action args Done!"
-            });*/
         }
         else {
             console.log('deleteActionArgs FAIL');
             res.status(401).json({
                 success: false,
                 body: "deletion of job's action args Failed!"
-                //body: 'deletion of job Failed'
             });
         }
     });
 
-
-
-/*
-    job.deleteJob(req.header('authtoken'), req.body.jobToken)
-    .catch((e) => {
-        isSuccess_3 = false;
-        console.log(e);
-    })
-    .then((user) => {
-        if (isSuccess_3 == true){
-            console.log('deleteJob SUCESSFUL');
-            res.status(200).json({
-                success: true,
-                body: 'deletion of job done!'
-            });
-        }
-        else {
-            console.log('deleteJob FAIL');
-            res.status(401).json({
-                success: false,
-                body: 'deletion of job Failed'
-            });
-        }
-    });
-*/
     console.log('delete-job');
     console.log('Got body:', req.body);
 };
--- server/src/controllers/job/job_extra.js
+++ server/src/controllers/job/job_extra.js
@@ -39,7 +39,7 @@ async function updateJobAddArgs(jobToken, actionArgJson, reactionArgJson) {
     return true;
 }
 
-async function updateJob_extra_3(jobToken, actionArgJson, reactionArgJson, req)
+async function updateJob_extra(jobToken, actionArgJson, reactionArgJson, req)
 {
     let is_failed = false;
 
@@ -67,12 +67,6 @@ async function updateJob_extra_3(jobToken, actionArgJson, reactionArgJson, req)
                     }
                 };
             }
-
-            /*return {code:401, json:{
-                success: false,
-                body: 'Try updateJobAddArgs Failed',
-                }
-            };*/
         }
         else {
 
@@ -134,251 +128,6 @@ async function updateJob_extra_3(jobToken, actionArgJson, reactionArgJson, req)
     }
 }
 
-async function updateJob_extra_2(jobToken, actionArgJson, reactionArgJson, req)
-{
-    let is_updateJobAddArgs = await updateJobAddArgs(jobToken, actionArgJson, reactionArgJson);
-
-    if (is_updateJobAddArgs) {
-        console.log("TRUE TRUE TRUE TRUE TRUE.");
-        let good_job = await job.findUniqueJob(jobToken);
-        if (good_job.jobToken && checkGetJob(req.body.action, actionArgJson, req.body.reaction, reactionArgJson)) {
-            console.log('updateJob and findUniqueJob SUCESSFUL');
-            scheduler.removeById(good_job.jobToken);
-
-            const jobToLaunch = getJob(req.body.action, actionArgJson, req.body.reaction, reactionArgJson);
-            const job1 = new SimpleIntervalJob(
-                { seconds: req.body.interval, runImmediately: req.body.runNow },
-                jobToLaunch,
-                good_job.jobToken
-            );
-            scheduler.addSimpleIntervalJob(job1);
-
-            return {code:200, json:{
-                success: true,
-                body: 'Update of job done!',
-                good_job
-                }
-            };
-
-        }
-        else if (good_job.jobToken) {
-            console.log('updateJob, checkGetJob FAIL');
-            scheduler.removeById(good_job.jobToken);
-            let deletedJob = await job.deleteJob(req.header('authtoken'), good_job.jobToken);
-            if (deletedJob.id) {
-                console.log('good_job deleteJob SUCESSFUL');
-                return {code:401, json:{
-                    success: false,
-                    body: 'good_job deletion of job done!'
-                    }
-                };
-            }
-            else {
-                return {code:401, json:{
-                    success: false,
-                    body: 'good_job deletion of job Failed'
-                    }
-                };
-            }
-        }
-        else {
-            console.log('good_job deleteJob FULL FAIL');
-            scheduler.removeById(good_job.jobToken);
-            return {code:401, json:{
-                success: false,
-                body: 'Update of job Failed'
-                }
-            };
-        }
-    }
-    /*else if (true) {
-        console.log("ok o ok ok ok ok.");
-    }*/
-    else {
-        console.log('updateJobAddArgs FAIL');
-        scheduler.removeById(jobToken);
-        let deletedJob = await job.deleteJob(req.header('authtoken'), jobToken);
-            if (deletedJob.id) {
-                console.log('updateJobAddArgs FAIL deleteJob SUCESSFUL');
-                return {code:401, json:{
-                    success: false,
-                    body: 'updateJobAddArgs deletion of job done!'
-                    }
-                };
-            }
-            else {
-                return {code:401, json:{
-                    success: false,
-                    body: 'updateJobAddArgs deletion of job Failed'
-                    }
-                };
-            }
-    }
-
-
-    // return {code:200, json:{
-    //     success: true,
-    //     body: 'Update of job done!'
-    //     }
-    // };
-}
-
-async function updateJob_extra(jobToken, actionArgJson, reactionArgJson, req)
-{
-    let isSuccess = true;
-    let isSuccess_2 = true;
-
-    updateJobAddArgs(jobToken, actionArgJson, reactionArgJson)
-    .catch((e) => {
-        console.log(e);
-    })
-    .then((arg_job) => {
-        //console.log('updateJob_extra Done Confirmed =', arg_job);
-        if (arg_job == true) {
-
-            job.findUniqueJob(jobToken)
-            .catch((e) => {
-                isSuccess = false;
-                console.log(e);
-            })
-            .then((good_job) => {
-                if (isSuccess == true && checkGetJob(req.body.action, actionArgJson, req.body.reaction, reactionArgJson)) {
-                    console.log('updateJob and findUniqueJob SUCESSFUL');
-
-
-/*
-                    res.status(200).json({
-                        success: true,
-                        body: 'Update of job done!',
-                        good_job
-                    });*/
-
-                    scheduler.removeById(good_job.jobToken);
-
-                    const jobToLaunch = getJob(req.body.action, actionArgJson, req.body.reaction, reactionArgJson);
-                    const job1 = new SimpleIntervalJob(
-                        { seconds: req.body.interval, runImmediately: req.body.runNow },
-                        jobToLaunch,
-                        good_job.jobToken
-                    );
-                    scheduler.addSimpleIntervalJob(job1);
-
-                    return 200, {
-                        success: true,
-                        body: 'Update of job done!',
-                        good_job
-                    };
-
-                }
-                else if (isSuccess == true) {
-                    console.log('updateJob, checkGetJob FAIL');
-
-                    scheduler.removeById(good_job.jobToken);
-
-                    job.deleteJob(req.header('authtoken'), good_job.jobToken)
-                    .catch((e) => {
-                        isSuccess_2 = false;
-                        console.log(e);
-                    })
-                    .then((user) => {
-                        if (isSuccess_2 == true) {
-                            console.log('good_job deleteJob SUCESSFUL');
-
-                            // res.status(401).json({
-                            //     success: true,
-                            //     body: 'good_job deletion of job done!'
-                            // });
-
-                            return 401, {
-                                success: true,
-                                body: 'Update of job failed, deletion of job done!'
-                            };
-
-                        }
-                        else {
-                            console.log('good_job deleteJob FAIL');
-                            // res.status(401).json({
-                            //     success: false,
-                            //     body: 'good_job deletion of job Failed'
-                            // });
-
-                            return 401, {
-                                success: false,
-                                body: 'good_job deletion of job Failed'
-                            };
-
-                        }
-                    });
-
-                }
-                else {
-                    console.log('good_job deleteJob FULL FAIL');
-
-                    scheduler.removeById(good_job.jobToken);
-
-                    // res.status(401).json({
-                    //     success: false,
-                    //     body: 'Update of job Failed'
-                    // });
-
-                    return 401, {
-                        success: false,
-                        body: 'Update of job Failed'
-                    };
-
-                }
-            });
-        }
-
-
-        //--//
-
-
-        else {
-            console.log('updateJobAddArgs FAIL');
-
-            scheduler.removeById(jobToken);
-
-
-            job.deleteJob(req.header('authtoken'), jobToken)
-            .catch((e) => {
-                isSuccess_2 = false;
-                console.log(e);
-            })
-            .then((user) => {
-                if (isSuccess_2 == true) {
-                    console.log('updateJobAddArgs FAIL deleteJob SUCESSFUL');
-
-                    // res.status(200).json({
-                    //     success: true,
-                    //     body: 'updateJobAddArgs deletion of job done!'
-                    // });
-
-                    return 200, {
-                        success: true,
-                        body: 'updateJobAddArgs deletion of job done!'
-                    };
-
-                }
-                else {
-                    console.log('updateJobAddArgs FAIL deleteJob FAIL');
-                    // res.status(401).json({
-                    //     success: false,
-                    //     body: 'updateJobAddArgs deletion of job Failed'
-                    // });
-
-                    return 401, {
-                        success: false,
-                        body: 'updateJobAddArgs deletion of job Failed'
-                    };
-
-                }
-            });
-        }
-    });
-
-}
-
 function checkGetJob(action, actionArgs, reaction, reactionArgs)
 {
     if (actions.checkAction.get(action)(actionArgs) == false || reactions.checkReaction.get(reaction)(reactionArgs) == false)
@@ -388,25 +137,9 @@ function checkGetJob(action, actionArgs, reaction, reactionArgs)
 
 function getJob(action, actionArgs, reaction, reactionArgs)
 {
-/*
-    actions.action.get(action)(actionArgs, reactions.reaction.get(reaction), reactionArgs);
-
-    var job = new SimpleIntervalJob(
-        { seconds: 20, runImmediately: true },
-        task,
-        'id_1'
-    );
-*/
-    /*const test = actions.action.get(action)(actionArgs, reactions.reaction.get(reaction), reactionArgs);*/
-    //return test;
-
     return new Task('test', () => {actions.action.get(action)(actionArgs, reactions.reaction.get(reaction), reactionArgs)});
-
-    //return actions.action.get(action)(actionArgs, reactions.reaction.get(reaction), reactionArgs);
 }
 
-//module.exports.updateJob_extra = updateJob_extra;
-//module.exports.updateJob_extra_2 = updateJob_extra_2;
-module.exports.updateJob_extra_3 = updateJob_extra_3;
+module.exports.updateJob_extra = updateJob_extra;
 module.exports.checkGetJob = checkGetJob;
 //module.exports.getJob = getJob;
