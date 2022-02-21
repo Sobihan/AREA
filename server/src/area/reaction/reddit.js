3e5cc41e0a29f372db8d32ebdba981c0d3733255
Maxime Verrier <maxime.verrier@epitech.eu>
2022-02-21 23:07:25 +0100

    [Add] basic pm reddit reaction

--- server/src/area/reaction/reddit.js
+++ server/src/area/reaction/reddit.js
@@ -1,6 +1,10 @@
 const fetch = require('node-fetch');
 const http_r = require('../../api/http_requester');
 
+async function send_request(token, url, data) {
+
+}
+
 async function submit_post(token, subreddit, title, text)
 {
     var url = "https://oauth.reddit.com/api/submit"
@@ -18,6 +22,29 @@ async function submit_post(token, subreddit, title, text)
             sr: subreddit,
             title: title,
             text: text,
+        }
+    };
+    var data = await http_r.apiCaller(opt, url);
+    return data;
+};
+
+async function send_pm(token, subject, text, to)
+{
+    var url = "https://oauth.reddit.com/api/compose"
+    var myHeaders = new fetch.Headers();
+
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", 'bearer ' + token);
+    myHeaders.append("user-agent", "node.js");
+    var opt = {
+        method: 'POST',
+        headers: myHeaders,
+        data : {
+            api_type: 'json',
+            kind: 'self',
+            subject: subreddit,
+            text: text,
+            to: to,
         }
     };
     var data = await http_r.apiCaller(opt, url);
