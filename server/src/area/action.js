f1eaeaa44eb9856c1933ac17a28d76da16675898
Adrien Moreau <adrien.moreau@epitech.eu>
2022-02-21 23:41:37 +0100

    [ADD] nasa APOD action

--- server/src/area/action.js
+++ server/src/area/action.js
@@ -1,11 +1,13 @@
 const twitch = require('./action/twitch');
 const poem = require('./action/poem');
+const nasa = require('./action/nasa');
 
 const action = new Map();
 
 action.set("getStream", twitch.getStream);
 action.set("testAction", twitch.testAction);
 action.set("getRandomPoem", poem.getRandomPoem);
+action.set("getAPOD", nasa.getAPOD);
 
 module.exports.action = action;
 
@@ -16,6 +18,7 @@ const checkAction = new Map();
 checkAction.set("getStream", twitch.checkGetStream);
 checkAction.set("testAction", twitch.checktestAction);
 checkAction.set("getRandomPoem", poem.checkGetRandomPoem);
+checkAction.set("getAPOD", nasa.checkGetAPOD);
 
 module.exports.checkAction = checkAction;
 
@@ -24,6 +27,7 @@ module.exports.checkAction = checkAction;
 const infoAction = new Map();
 
 infoAction.set("twitch", {name: "twitch", actions: twitch.twitchInfo});
-infoAction.set("poem", {name: "poem", reactions: poem.poemInfo});
+infoAction.set("poem", {name: "poem", actions: poem.poemInfo});
+infoAction.set("nasa", {name: "nasa", actions: nasa.nasaInfo});
 
 module.exports.infoAction = infoAction;
