cef4fa4c73b75d0c70c6b3fe95aa21d35c430e4d
Maxime Verrier <maxime.verrier@epitech.eu>
2022-02-21 23:12:12 +0100

    [Add] abstract to reddit request

--- server/src/area/reaction/reddit.js
+++ server/src/area/reaction/reddit.js
@@ -2,12 +2,6 @@ const fetch = require('node-fetch');
 const http_r = require('../../api/http_requester');
 
 async function send_request(token, url, data) {
-
-}
-
-async function submit_post(token, subreddit, title, text)
-{
-    var url = "https://oauth.reddit.com/api/submit"
     var myHeaders = new fetch.Headers();
 
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
@@ -16,37 +10,32 @@ async function submit_post(token, subreddit, title, text)
     var opt = {
         method: 'POST',
         headers: myHeaders,
-        data : {
+        data : data
+    };
+    var data = await http_r.apiCaller(opt, url);
+    return data;
+}
+
+async function submit_post(token, subreddit, title, text)
+{
+    return send_request(token, "https://oauth.reddit.com/api/submit", {
             api_type: 'json',
             kind: 'self',
             sr: subreddit,
             title: title,
             text: text,
         }
-    };
-    var data = await http_r.apiCaller(opt, url);
-    return data;
+    );
 };
 
 async function send_pm(token, subject, text, to)
 {
-    var url = "https://oauth.reddit.com/api/compose"
-    var myHeaders = new fetch.Headers();
-
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append("Authorization", 'bearer ' + token);
-    myHeaders.append("user-agent", "node.js");
-    var opt = {
-        method: 'POST',
-        headers: myHeaders,
-        data : {
-            api_type: 'json',
-            kind: 'self',
-            subject: subreddit,
-            text: text,
-            to: to,
-        }
-    };
-    var data = await http_r.apiCaller(opt, url);
-    return data;
+    return send_request(token, "https://oauth.reddit.com/api/compose", {
+        api_type: 'json',
+        kind: 'self',
+        subject: subreddit,
+        text: text,
+        to: to,
+    }
+    );
 };
