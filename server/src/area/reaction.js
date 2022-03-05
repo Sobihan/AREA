2e5f5e9d9b2fa2700c244a060990bfe221624f9d
Adrien Moreau <adrien.moreau@epitech.eu>
2022-03-05 20:39:20 +0100

    [CLEAN]

--- server/src/area/reaction.js
+++ server/src/area/reaction.js
@@ -1,14 +1,9 @@
 const test = require('./reaction/test');
-// const mail = require('./reaction/mail');
-// const reddit = require('./reaction/reddit');
 const discord = require('./reaction/discord');
 
 const reaction = new Map();
 
 reaction.set("testReaction", test.testReaction);
-// reaction.set("sendMail", mail.sendMailReaction);
-// reaction.set("redditPost", reddit.submitRedditPost);
-// reaction.set("redditPM", reddit.sendPM);
 reaction.set("sendServerMessages", discord.sendServerMessages);
 reaction.set("sendPrivateMessages", discord.sendPrivateMessages);
 
@@ -19,9 +14,6 @@ module.exports.reaction = reaction;
 const checkReaction = new Map();
 
 checkReaction.set("testReaction", test.checkTestReaction);
-// checkReaction.set("sendMail", mail.checkSendMail);
-// checkReaction.set("redditPost", reddit.checkSubmitRedditPost);
-// checkReaction.set("redditPM", reddit.checkSendPM);
 checkReaction.set("sendServerMessages", discord.checkSendServerMessages);
 checkReaction.set("sendPrivateMessages", discord.checkSendPrivateMessages);
 
@@ -31,9 +23,6 @@ module.exports.checkReaction = checkReaction;
 
 const infoReaction = new Map();
 
-infoReaction.set("testInfo", { name: "testInfo", reactions: test.testInfo });
-// infoReaction.set("mailInfo", { name: "mailInfo", reactions: mail.mailInfo });
-// infoReaction.set("redditInfo", { name: "mailInfo", reactions: reddit.redditInfo });
 //infoReaction.set("testInfo", {name: "testInfo", reactions: test.testInfo});
 infoReaction.set("twitch", {name: "twitch", reactions: test.testInfo});
 infoReaction.set("discord", {name: "discord", reactions: discord.discordInfo});
--- server/src/tests/mail.js
+++
@@ -1,8 +0,0 @@
-// var mail = require("../area/reaction/mail.js")
-
-// function main() {
-//     console.log("testab");
-
-//     mail.sendMailReaction(["smtp.gmail.com", "*redacted*", "*redacted*", "maxime.verrier@epitech.eu", "test", "test body"]);
-// }
-// main()
